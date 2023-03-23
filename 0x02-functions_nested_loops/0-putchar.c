6d59a8b5173695d75b7f53f51311a7a4ed23b518
iamgeorgeokudo <okudog1@gmail.com>
2023-03-23 03:52:53 +0300

    This is th  first commit for loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	_putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return 0;
+}
+int _putchar(char c){
+	return putchar(c);
+}
